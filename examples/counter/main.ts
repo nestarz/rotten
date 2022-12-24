0e6d655da5583c987c169b96d26565502102db23
Elias Rhouzlane <elias.rhouzlane@protonmail.com>
2022-12-24 21:20:43 +0100

    fix: simpler example

--- examples/counter/main.ts
+++ examples/counter/main.ts
@@ -1,10 +1,27 @@
 import { serve } from "std/http/server.ts";
 import { router } from "rutt";
-import { withRenderer, withCtx } from "rotten";
+import { withRenderer, withCtx, svgbuild, stylesbuild } from "rotten";
 import { islandsRot, stylesRot, staticRot } from "rotten/routes";
 
 import * as stylesGen from "./styles.gen.ts";
 import * as Home from "./routes/Home.tsx";
+
+if (!Deno.env.get("DENO_DEPLOYMENT_ID")) {
+  await svgbuild({
+    outdir: "./components/svg/",
+    outfile: "./svg.gen.ts",
+    entries: [
+      {
+        base: "https://esm.sh/remixicon@2.5.0/icons/",
+        urls: ["Document/folder-2-line.svg"],
+      },
+    ],
+  });
+  await stylesbuild({
+    entrydir: new URL("./styles/", import.meta.url),
+    outfile: new URL("./styles.gen.ts", import.meta.url),
+  });
+}
 
 const styles = await stylesRot.setup({
   stylesGen,
